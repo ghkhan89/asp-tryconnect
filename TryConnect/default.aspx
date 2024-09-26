23a565f17355e9145e9e30bbfc4e36006c0f8d58
Ghufran Khan <ghkhan89@gmail.com>
2024-09-26 22:45:28 +0530

    RawFiles

---
+++ TryConnect/default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TryConnect._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>TryConnect</title>
+    <style type="text/css">
+        .auto-style3 {
+            height: 38px;
+        }
+        .auto-style4 {
+            height: 44px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table cellpadding="1">
+                <tr><td class="auto-style3" style="text-align: center; font-size: 20px;" colspan=2>Try Connect</td></tr>
+                <tr><td class="auto-style4">Enter your connection string</td><td class="auto-style4"><asp:TextBox ID="ConString" runat="server"  Width="1109px" ToolTip="Enter your connection string " Height="30px"></asp:TextBox></td></tr>
+                <tr><td colspan=2><asp:Button ID="BtnTryConnect" runat="server" OnClick="BtnTryConnect_Click" Text="Connect"  Width="1304px" Height="40px"/></td></tr>
+                <tr><td colspan=2><asp:TextBox ID="ExTxtbox" runat="server" TextMode="MultiLine" ReadOnly="True"  Width="1299px" Height="278px"></asp:TextBox></td></tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TryConnect/default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TryConnect
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnTryConnect_Click(object sender, EventArgs e)
+        {
+            StringBuilder result = new StringBuilder();
+            try
+            {
+                if (ConString.Text != "")
+                {
+                    var connection = new SqlConnection(ConString.Text.ToString());
+                    connection.Open();
+
+                    var sql = "SELECT SERVERPROPERTY('MachineName') AS 'MachineName', SERVERPROPERTY('Edition') AS 'Edition', SERVERPROPERTY('ProductVersion') AS 'ProductVersion';";
+                    var command = new SqlCommand(sql, connection);
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        result.Append(reader.GetName(0) + ":" + reader.GetString(0) + Environment.NewLine);
+                        result.Append(reader.GetName(1) + ":" + reader.GetString(1) + Environment.NewLine);
+                        result.Append(reader.GetName(2) + ":" + reader.GetString(2) + Environment.NewLine);
+                    }
+
+                    ExTxtbox.Text = result.ToString();
+                    command.Dispose();
+                    connection.Close();
+                    connection.Dispose();
+                }
+                else
+                {
+                    ExTxtbox.Text = "Enter connection string please";
+                }
+            }
+            catch (SqlException esql)
+            {
+                ExTxtbox.Text = "Exception while Querying SQL:" + " \n\n-------------------------\n\n "
+                    + esql.Server.ToString() + " \n\n-------------------------\n\n "
+                    + esql.Message.ToString() + " \n\n-------------------------\n\n "
+                    + esql.ToString();
+            }
+        }
+    }
+}
